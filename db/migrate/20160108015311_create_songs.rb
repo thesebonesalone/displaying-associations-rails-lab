bd15a99a29872fea5f5779318fedcf3e110e0b23
thesebonesalone <duikhead2@gmail.com>
2020-10-14 10:17:30 -0500

    Done.

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
+      t.belongs_to :artist
 
       t.timestamps null: false
     end
